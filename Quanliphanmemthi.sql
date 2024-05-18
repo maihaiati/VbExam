0ab79c76762e12fba8948dbbe9427eda2415b88a
Ngotuanviet <80836103+ngotuanviet@users.noreply.github.com>
2024-05-18 21:16:26 +0700

    Update file DB

--- Quanliphanmemthi.sql
+++ Quanliphanmemthi.sql
@@ -1,6 +1,6 @@
 ﻿CREATE DATABASE ExamDB
 use ExamDB
-create table sinhvien(
+create table Sinhvien(
 	Masv nvarchar(60) not null,
 	HoTen Nvarchar(100),
 	Gioitinh nvarchar(10),
@@ -9,34 +9,46 @@ create table sinhvien(
 	Khoa nvarchar(100)
 	constraint sinhvien_id primary key (Masv)
 )
-create table tksv(
-	usersv nvarchar(40),
-	passsv nvarchar(40)
+create table Tksv(
+	Usersv nvarchar(40),
+	Passsv nvarchar(40)
 	)
 	insert into tksv(usersv,passsv) values (2823220212,N'SV123@')
-create table tkgv(
-	usergv nvarchar(40),
-	passgv nvarchar(40)
-
+create table Tkgv(
+	Usergv nvarchar(40),
+	Passgv nvarchar(40)
 )
 	insert into tkgv(usergv,passgv) values ('GV001',N'GV123@')
 create table Giangvien(
-	magv nvarchar(60) not null,
-	hotengv nvarchar(100),
-	chucvu nvarchar(50),
+	Magv nvarchar(60) not null primary key,
+	Hotengv nvarchar(100),
+	Chucvu nvarchar(50),
 	Khoa nvarchar(50)
 )
-	insert into Giangvien(magv,hotengv,chucvu,Khoa) values ('GV123',N'Mai Quang Hải',N'Giảng Viên',N'Công Nghệ thông tin')
+	insert into Giangvien(Magv,Hotengv,Chucvu,Khoa) values('GV123',N'Mai Quang Hải','Giảng viên','Công nghệ thông tin')
 	 insert into sinhvien(Masv,HoTen,Gioitinh,Ngaysinh,Lop,Khoa) values (2823220212,N'Ngô Tuấn Việt','Nam',2005-12-12,'TH28.27',N'Công Nghệ Thông Tin')
-Create table ketquathi(
-	Mamonthi nvarchar(60) not null primary key,
+Create table Ketquathi(
+	Mamonhoc nvarchar(60) not null,
 	Masv nvarchar(60) not null,
 	Tenmonthi nvarchar(100),
 	Phongthi nvarchar(100),
-	diemkt int not null,
-	diemchuyencan int not null,
-	diemthi int not null,
+	Diemkt int not null,
+	Diemchuyencan int not null,
+	Diemthi int not null,
 	constraint fk_masv foreign key (Masv) references sinhvien(Masv)
+)
+use ExamDB
+CREATE TABLE Monhoc(
+	Mamonhoc nvarchar(60) not null primary key,
+	Tenmonhoc nvarchar(60) not null,
+)
+CREATE TABLE Lichthi(
+	Mamonhoc nvarchar(60) not null, 
+	Masv nvarchar(60) not null,
+	Tenmonhoc nvarchar(60),
+	Lop nvarchar(10) ,
+	Ngaythi datetime,
+	Phongthi nvarchar(10)
 )
 CREATE TABLE Khoa (
 	MaKhoa NVARCHAR(10) NOT NULL PRIMARY KEY,
@@ -76,5 +88,5 @@ VALUES	('DATIN1', 'TIN1', N'Đáp án câu 1'),
 		('DATIN2', 'TIN2', N'Đáp án câu 2')
 
 		delete ketquathi
-	insert into ketquathi(Mamonthi,Masv,Tenmonthi,Phongthi,diemkt,diemchuyencan,diemthi) values ('DHMT',2823220212,N'Đồ hoạ máy tính','D604',8,0.2,9)
+	insert into ketquathi(Mamonhoc,Masv,Tenmonthi,Phongthi,diemkt,diemchuyencan,diemthi) values ('DHMT',2823220212,N'Đồ hoạ máy tính','D604',8,0.2,9)
 	select sinhvien.Masv,HoTen,Gioitinh,Ngaysinh,Lop,Khoa from tksv inner join sinhvien on tksv.usersv = sinhvien.Masv where Masv = '2823220212'
