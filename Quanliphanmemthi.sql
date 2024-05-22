6b0621880a4200ca4e5d646dffb4ead63b5bf7fa
Ngotuanviet <80836103+ngotuanviet@users.noreply.github.com>
2024-05-22 10:46:09 +0700

    update DB

--- Quanliphanmemthi.sql
+++ Quanliphanmemthi.sql
@@ -3,28 +3,33 @@ use ExamDB
 create table Sinhvien(
 	Masv nvarchar(60) not null,
 	Passsv nvarchar(60) not null,
+	imagesv varbinary(max),
 	HoTen Nvarchar(100) not null,
 	Gioitinh nvarchar(10) not null,
 	Ngaysinh date not null,
 	Lop nvarchar(20) not null,
 	Khoa nvarchar(100) not null
 	constraint sinhvien_id primary key (Masv)
-)
-insert into Sinhvien(Masv,Passsv,HoTen,Gioitinh,Ngaysinh,Lop,Khoa) values 
-(2823220212,'282322012',N'Ngô Tuấn Việt',N'Nam','2005-12-12',N'TH28.27',N'Công Nghệ Thông Tin')
+	)
+	use ExamDB
+insert into Sinhvien(Masv,Passsv,imagesv,HoTen,Gioitinh,Ngaysinh,Lop,Khoa) values 
+('2823220212',2823220212,(SELECT * FROM OPENROWSET(BULK 'C:\Users\ntv\Downloads\imagesv\2823220212.jpg', SINGLE_BLOB) AS image),N'Ngô Tuấn Việt',N'Nam','2005-12-12',N'TH28.27',N'Công Nghệ Thông Tin')
 
+select * from Loginfo
+delete Loginfo
 create table Loginfo(
 	Tennguoidung nvarchar(55) not null,
 	Hoatdong nvarchar(40) not null,
 	Trangthai nvarchar(40) not null,
 	thoigian datetime not null,
 	chitiet nvarchar(40)
-)
+) 
 insert into Loginfo(Tennguoidung,Hoatdong,Trangthai,thoigian,chitiet) values (N'Admin',N'Đăng Nhập',N'thành công',GETDATE(),null);
 use ExamDB
 create table Giangvien(
 	Magv nvarchar(60) not null primary key,
 	Passgv nvarchar(60) not null,
+	imagegv varbinary(max),
 	Hotengv nvarchar(100) not null,
 	Gioitinh nvarchar(10) not null,
 	Ngaysinh date not null,
@@ -32,7 +37,9 @@ create table Giangvien(
 	Khoa nvarchar(50) not null,
 	Administrator BIT not null,
 )
-insert into Giangvien(Magv,Passgv,Hotengv,Gioitinh,Ngaysinh,Chucvu,Khoa,Administrator) values ('GV012@','GV012@',N'Mai Quang Hải',N'Nam','07/16/2005',N'Trưởng Khoa',N'Cộng Nghệ Thông Tin', 1)
+use ExamDB
+insert into Giangvien(Magv,Passgv,imagegv,Hotengv,Gioitinh,Ngaysinh,Chucvu,Khoa,Administrator) values ('GV012@','GV012@',(SELECT * FROM OPENROWSET(BULK 'C:\Users\ntv\Downloads\imagegv\GV012@.jpg', SINGLE_BLOB) AS image),N'Mai Quang Hải',N'Nam','07/16/2005',N'Trưởng Khoa',N'Cộng Nghệ Thông Tin', 1)
+
 Create table Bangdiem(
 	Mamonhoc nvarchar(60) not null,
 	Masv nvarchar(60) not null,
@@ -55,9 +62,10 @@ CREATE TABLE Lichthi(
 	Ngaythi datetime not null,
 	Phongthi nvarchar(10) not null
 )
+use ExamDB
 insert into Lichthi(Mamonhoc,Masv,Tenmonhoc,Lop,Ngaythi,Phongthi) 
-values ('DHMT',2823220212,N'Đồ Hoạ Máy Tính','TH28.27',26-06-2023,'D509') 
-
+values ('DHMT',2823220212,N'Đồ Hoạ Máy Tính','TH28.27','2023-04-05 09:30:00','D509')
+		
 CREATE TABLE Khoa (
 	MaKhoa NVARCHAR(10) NOT NULL PRIMARY KEY,
 	TenKhoa NVARCHAR(30) NOT NULL
@@ -76,9 +84,16 @@ CREATE TABLE CauHoi (
 	NoiDung NVARCHAR(MAX) NOT NULL,
 	ThuTuCau int NOT NULL
 )
+
 INSERT INTO CauHoi (MaCauHoi, MaDeThi, NoiDung, ThuTuCau) 
 VALUES  ('TIN1', 'TIN01', N'Câu hỏi thứ nhất', 1),
 		('TIN2', 'TIN01', N'Câu hỏi thứ hai', 2)
+
+create table Imagecauhoi(
+	MaCauHoi nvarchar(40) not null,
+	imagech varbinary (max)
+)
+insert into Imagecauhoi(MaCauHoi,imagech) values ('TIN1',(SELECT * FROM OPENROWSET(BULK 'C:\Users\ntv\Downloads\imagesv\2823220212.jpg', SINGLE_BLOB) AS image))
 
 CREATE TABLE DapAn (
 	MaDapAn NVARCHAR(10) NOT NULL PRIMARY KEY,
@@ -88,3 +103,18 @@ CREATE TABLE DapAn (
 INSERT INTO DapAn (MaDapAn, MaCauHoi, NoiDung)
 VALUES	('DATIN1', 'TIN1', N'Đáp án câu 1'),
 		('DATIN2', 'TIN2', N'Đáp án câu 2')
+		insert into Loginfo(Tennguoidung,Hoatdong,Trangthai,thoigian,chitiet) values (N'Admin',N'Đăng Nhập',N'thành công','2024-05-30',null);
+
+
+
+	CREATE PROCEDURE DeleteOldData
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE FROM Loginfo WHERE thoigian < DATEADD(WEEK, -1, GETDATE());
+END
+EXEC DeleteOldData;
+select * from Loginfo
+use ExamDB
+select * from Giangvien
