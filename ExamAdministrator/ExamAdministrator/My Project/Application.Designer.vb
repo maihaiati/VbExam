f4260e16ee09a1eeebab08bc71ac6b04e4b4f748
M2Tech <maiquanghai011@gmail.com>
2024-05-23 18:44:56 +0700

    Fix merge conflict

--- ExamAdministrator/ExamAdministrator/My Project/Application.Designer.vb
+++ ExamAdministrator/ExamAdministrator/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ExamAdministrator.ExamSchedule
+            Me.MainForm = Global.ExamAdministrator.LoginForm
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
