230be895d1ee8ce8f16a570080c8449a38fc4451
M2Tech <maiquanghai011@gmail.com>
2024-05-29 22:28:43 +0700

    Update ExamSubject

--- ExamAdministrator/ExamAdministrator/ExamSubject.vb
+++ ExamAdministrator/ExamAdministrator/ExamSubject.vb
@@ -8,36 +8,34 @@ Public Class ExamSubject
         LoadData()
     End Sub
     Private Sub LoadData()
-        Dim query As String = "SELECT * FROM MonHoc"
-        Dim adapter As New SqlDataAdapter(query, connection)
-        Dim table As New DataTable()
-        adapter.Fill(table)
-        DGVMonhoc.DataSource = table
+        assignData(DGVMonhoc, "SELECT * FROM MonHoc", Nothing)
     End Sub
     Private Sub btnthem_Click(sender As Object, e As EventArgs) Handles btnthem.Click
         Dim query As String = "INSERT INTO MonHoc (Mamonhoc, Tenmonhoc,Sotiet) VALUES (@MaMonHoc, @TenMonHoc, @SoTiet)"
-        Dim command As New SqlCommand(query, connection)
-        command.Parameters.AddWithValue("@MaMonHoc", txtmamh.Text)
-        command.Parameters.AddWithValue("@TenMonHoc", txttmh.Text)
-        command.Parameters.AddWithValue("@SoTiet", txtsotiet.Text)
-        connection.Open()
-        command.ExecuteNonQuery()
-        connection.Close()
+        Dim params As New List(Of SqlParameter)
+		params.Add(New SqlParameter("@MaMonHoc", txtmamh.Text))
+        params.Add(New SqlParameter("@TenMonHoc", txttmh.Text))
+        params.Add(New SqlParameter("@SoTiet", txtsotiet.Text))
+        If runSqlCommand(query, params) Then
+            MessageBox.Show("Thêm thành công!", "Exam Administrator", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Thêm thất bại!", "Exam Administrator", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
         LoadData()
-        MsgBox("Thêm Thành Công!", MsgBoxStyle.Information)
         txtmamh.Text = ""
         txttmh.Text = ""
         txtsotiet.Text = ""
     End Sub
     Private Sub btnxoa_Click(sender As Object, e As EventArgs) Handles btnxoa.Click
         Dim query As String = "DELETE FROM MonHoc WHERE MaMonHoc = @MaMonHoc"
-        Dim command As New SqlCommand(query, connection)
-        command.Parameters.AddWithValue("@MaMonHoc", txtmamh.Text)
-        connection.Open()
-        command.ExecuteNonQuery()
-        connection.Close()
+        Dim params As New List(Of SqlParameter)
+        params.Add(New SqlParameter("@MaMonHoc", txtmamh.Text))
+        If runSqlCommand(query, params) Then
+            MessageBox.Show("Xoá Thành Công!", "Exam Administrator", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Xoá Thất Bại!", "Exam Administrator", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
         LoadData()
-        MsgBox("Xoá Thành Công!", MsgBoxStyle.Information)
         txtmamh.Text = ""
         txttmh.Text = ""
         txtsotiet.Text = ""
@@ -45,38 +43,35 @@ Public Class ExamSubject
 
     Private Sub btnchinhsua_Click(sender As Object, e As EventArgs) Handles btnchinhsua.Click
         Dim query As String = "UPDATE MonHoc SET TenMonHoc = @TenMonHoc,SoTiet = @SoTiet WHERE MaMonHoc = @MaMonHoc"
-        Dim command As New SqlCommand(query, connection)
-        command.Parameters.AddWithValue("@MaMonHoc", txtmamh.Text)
-        command.Parameters.AddWithValue("@TenMonHoc", txttmh.Text)
-        command.Parameters.AddWithValue("@SoTiet", txtsotiet.Text)
-        connection.Open()
-        command.ExecuteNonQuery()
-        connection.Close()
+        Dim params As New List(Of SqlParameter)
+        params.Add(New SqlParameter("@MaMonHoc", txtmamh.Text))
+        params.Add(New SqlParameter("@TenMonHoc", txttmh.Text))
+        params.Add(New SqlParameter("@SoTiet", txtsotiet.Text))
+        If runSqlCommand(query, params) Then
+            MessageBox.Show("Cập Nhập Thành Công!", "Exam Administrator", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Cập Nhập Thất Bại!", "Exam Administrator", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
         LoadData()
-        MsgBox("Cập Nhập Thành Công!", MsgBoxStyle.Information)
         txtmamh.Text = ""
         txttmh.Text = ""
         txtsotiet.Text = ""
     End Sub
 
-    Private Sub DGVMonhoc_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGVMonhoc.CellContentDoubleClick
+    Private Sub txttimkiem_KeyUp(sender As Object, e As KeyEventArgs) Handles txttimkiem.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            Dim params As New List(Of SqlParameter)
+            params.Add(New SqlParameter("@MaMonHoc", txttimkiem.Text))
+            assignData(DGVMonhoc, "SELECT * FROM MonHoc WHERE MaMonHoc LIKE '%' + @MaMonHoc + '%'", params)
+        End If
+    End Sub
+
+    Private Sub DGVMonhoc_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DGVMonhoc.CellMouseClick
         If e.RowIndex >= 0 Then
             Dim row As DataGridViewRow = DGVMonhoc.Rows(e.RowIndex)
             txtmamh.Text = row.Cells("MaMonHoc").Value.ToString()
             txttmh.Text = row.Cells("TenMonHoc").Value.ToString()
             txtsotiet.Text = row.Cells("SoTiet").Value.ToString()
-        End If
-    End Sub
-
-    Private Sub txttimkiem_KeyUp(sender As Object, e As KeyEventArgs) Handles txttimkiem.KeyUp
-        If e.KeyCode = Keys.Enter Then
-            Dim query As String = "SELECT * FROM MonHoc WHERE MaMonHoc LIKE '%' + @MaMonHoc + '%'"
-            Dim command As New SqlCommand(query, connection)
-            command.Parameters.AddWithValue("@MaMonHoc", txttimkiem.Text)
-            Dim adapter As New SqlDataAdapter(command)
-            Dim table As New DataTable()
-            adapter.Fill(table)
-            DGVMonhoc.DataSource = table
         End If
     End Sub
 End Class
