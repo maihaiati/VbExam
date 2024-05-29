7528161c021011e611695869c2a2eb1f5722b46d
ngotuanviet <ngotuanviet9@gmail.com>
2024-05-29 11:00:55 +0700

    update style

--- ExamAdministrator/ExamAdministrator/ExamSchedule.Designer.vb
+++ ExamAdministrator/ExamAdministrator/ExamSchedule.Designer.vb
@@ -23,14 +23,9 @@ Partial Class ExamSchedule
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         MenuStrip1 = New MenuStrip()
-        TạoLịchToolStripMenuItem = New ToolStripMenuItem()
-        ThayĐổiLịchThiToolStripMenuItem = New ToolStripMenuItem()
-        XóaLịchToolStripMenuItem = New ToolStripMenuItem()
-        SắpXếpLịchThiToolStripMenuItem = New ToolStripMenuItem()
         ThoátToolStripMenuItem = New ToolStripMenuItem()
         DataGridView1 = New DataGridView()
         clnMMH = New DataGridViewTextBoxColumn()
-        clnMSV = New DataGridViewTextBoxColumn()
         clnMon = New DataGridViewTextBoxColumn()
         clnLop = New DataGridViewTextBoxColumn()
         clnNgayThi = New DataGridViewTextBoxColumn()
@@ -39,12 +34,17 @@ Partial Class ExamSchedule
         Label2 = New Label()
         Label3 = New Label()
         Label4 = New Label()
-        Button1 = New Button()
         TextBox1 = New TextBox()
         TextBox2 = New TextBox()
         TextBox3 = New TextBox()
         DateTimePicker1 = New DateTimePicker()
         Label5 = New Label()
+        TextBox4 = New TextBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Label6 = New Label()
         MenuStrip1.SuspendLayout()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
@@ -52,36 +52,12 @@ Partial Class ExamSchedule
         ' MenuStrip1
         ' 
         MenuStrip1.ImageScalingSize = New Size(20, 20)
-        MenuStrip1.Items.AddRange(New ToolStripItem() {TạoLịchToolStripMenuItem, ThayĐổiLịchThiToolStripMenuItem, XóaLịchToolStripMenuItem, SắpXếpLịchThiToolStripMenuItem, ThoátToolStripMenuItem})
+        MenuStrip1.Items.AddRange(New ToolStripItem() {ThoátToolStripMenuItem})
         MenuStrip1.Location = New Point(0, 0)
         MenuStrip1.Name = "MenuStrip1"
-        MenuStrip1.Size = New Size(839, 28)
+        MenuStrip1.Size = New Size(1297, 28)
         MenuStrip1.TabIndex = 0
         MenuStrip1.Text = "MenuStrip1"
-        ' 
-        ' TạoLịchToolStripMenuItem
-        ' 
-        TạoLịchToolStripMenuItem.Name = "TạoLịchToolStripMenuItem"
-        TạoLịchToolStripMenuItem.Size = New Size(102, 24)
-        TạoLịchToolStripMenuItem.Text = "Tạo Lịch Thi"
-        ' 
-        ' ThayĐổiLịchThiToolStripMenuItem
-        ' 
-        ThayĐổiLịchThiToolStripMenuItem.Name = "ThayĐổiLịchThiToolStripMenuItem"
-        ThayĐổiLịchThiToolStripMenuItem.Size = New Size(136, 24)
-        ThayĐổiLịchThiToolStripMenuItem.Text = "Thay Đổi Lịch Thi"
-        ' 
-        ' XóaLịchToolStripMenuItem
-        ' 
-        XóaLịchToolStripMenuItem.Name = "XóaLịchToolStripMenuItem"
-        XóaLịchToolStripMenuItem.Size = New Size(79, 24)
-        XóaLịchToolStripMenuItem.Text = "Xóa Lịch"
-        ' 
-        ' SắpXếpLịchThiToolStripMenuItem
-        ' 
-        SắpXếpLịchThiToolStripMenuItem.Name = "SắpXếpLịchThiToolStripMenuItem"
-        SắpXếpLịchThiToolStripMenuItem.Size = New Size(132, 24)
-        SắpXếpLịchThiToolStripMenuItem.Text = "Sắp Xếp Lịch Thi"
         ' 
         ' ThoátToolStripMenuItem
         ' 
@@ -92,11 +68,11 @@ Partial Class ExamSchedule
         ' DataGridView1
         ' 
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Columns.AddRange(New DataGridViewColumn() {clnMMH, clnMSV, clnMon, clnLop, clnNgayThi, clnPhong})
-        DataGridView1.Location = New Point(0, 269)
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {clnMMH, clnMon, clnLop, clnNgayThi, clnPhong})
+        DataGridView1.Location = New Point(12, 155)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowHeadersWidth = 51
-        DataGridView1.Size = New Size(839, 333)
+        DataGridView1.Size = New Size(805, 469)
         DataGridView1.TabIndex = 1
         ' 
         ' clnMMH
@@ -105,13 +81,6 @@ Partial Class ExamSchedule
         clnMMH.MinimumWidth = 6
         clnMMH.Name = "clnMMH"
         clnMMH.Width = 125
-        ' 
-        ' clnMSV
-        ' 
-        clnMSV.HeaderText = "MSV"
-        clnMSV.MinimumWidth = 6
-        clnMSV.Name = "clnMSV"
-        clnMSV.Width = 125
         ' 
         ' clnMon
         ' 
@@ -146,7 +115,7 @@ Partial Class ExamSchedule
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
         Label1.ForeColor = Color.Blue
-        Label1.Location = New Point(31, 119)
+        Label1.Location = New Point(887, 162)
         Label1.Name = "Label1"
         Label1.Size = New Size(102, 20)
         Label1.TabIndex = 2
@@ -157,7 +126,7 @@ Partial Class ExamSchedule
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
         Label2.ForeColor = Color.Blue
-        Label2.Location = New Point(446, 119)
+        Label2.Location = New Point(888, 284)
         Label2.Name = "Label2"
         Label2.Size = New Size(39, 20)
         Label2.TabIndex = 3
@@ -168,7 +137,7 @@ Partial Class ExamSchedule
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
         Label3.ForeColor = Color.Blue
-        Label3.Location = New Point(31, 182)
+        Label3.Location = New Point(887, 225)
         Label3.Name = "Label3"
         Label3.Size = New Size(106, 20)
         Label3.TabIndex = 4
@@ -179,40 +148,29 @@ Partial Class ExamSchedule
         Label4.AutoSize = True
         Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
         Label4.ForeColor = Color.Blue
-        Label4.Location = New Point(446, 182)
+        Label4.Location = New Point(888, 347)
         Label4.Name = "Label4"
         Label4.Size = New Size(84, 20)
         Label4.TabIndex = 5
         Label4.Text = "Phòng Thi:"
         ' 
-        ' Button1
-        ' 
-        Button1.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
-        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
-        Button1.Location = New Point(664, 42)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(107, 37)
-        Button1.TabIndex = 6
-        Button1.Text = "Tìm Kiếm"
-        Button1.UseVisualStyleBackColor = False
-        ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(156, 112)
+        TextBox1.Location = New Point(1012, 155)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(172, 27)
         TextBox1.TabIndex = 7
         ' 
         ' TextBox2
         ' 
-        TextBox2.Location = New Point(539, 175)
+        TextBox2.Location = New Point(1012, 343)
         TextBox2.Name = "TextBox2"
         TextBox2.Size = New Size(172, 27)
         TextBox2.TabIndex = 8
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New Point(539, 116)
+        TextBox3.Location = New Point(1012, 284)
         TextBox3.Name = "TextBox3"
         TextBox3.Size = New Size(172, 27)
         TextBox3.TabIndex = 9
@@ -221,7 +179,7 @@ Partial Class ExamSchedule
         ' 
         DateTimePicker1.CustomFormat = "hh:mm dd-MM-yyyy"
         DateTimePicker1.Format = DateTimePickerFormat.Custom
-        DateTimePicker1.Location = New Point(156, 177)
+        DateTimePicker1.Location = New Point(1012, 220)
         DateTimePicker1.Name = "DateTimePicker1"
         DateTimePicker1.Size = New Size(172, 27)
         DateTimePicker1.TabIndex = 10
@@ -232,23 +190,99 @@ Partial Class ExamSchedule
         Label5.AutoSize = True
         Label5.Font = New Font("Segoe UI", 12F, FontStyle.Bold Or FontStyle.Underline)
         Label5.ForeColor = Color.Red
-        Label5.Location = New Point(52, 42)
+        Label5.Location = New Point(542, 48)
         Label5.Name = "Label5"
         Label5.Size = New Size(294, 28)
         Label5.TabIndex = 11
         Label5.Text = "Thông Tin Danh Sách Lịch Thi"
+        Label5.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(152, 109)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(173, 27)
+        TextBox4.TabIndex = 12
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button2.BackgroundImageLayout = ImageLayout.None
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button2.Location = New Point(897, 510)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(143, 45)
+        Button2.TabIndex = 13
+        Button2.Text = "Xoá Lịch"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button3.BackgroundImageLayout = ImageLayout.None
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button3.Location = New Point(1060, 510)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(143, 45)
+        Button3.TabIndex = 14
+        Button3.Text = "Sắp xếp"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button4.BackgroundImageLayout = ImageLayout.None
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button4.Location = New Point(897, 442)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(143, 45)
+        Button4.TabIndex = 15
+        Button4.Text = "Tạo lịch thi"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button5.BackgroundImageLayout = ImageLayout.None
+        Button5.FlatStyle = FlatStyle.Flat
+        Button5.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Button5.Location = New Point(1060, 442)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(143, 45)
+        Button5.TabIndex = 16
+        Button5.Text = "Thay Đổi Lịch Thi"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label6.ForeColor = Color.Blue
+        Label6.Location = New Point(12, 112)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(138, 20)
+        Label6.TabIndex = 17
+        Label6.Text = "Tìm Kiếm Lịch Thi:"
         ' 
         ' ExamSchedule
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(839, 604)
+        ClientSize = New Size(1297, 649)
+        Controls.Add(Label6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(TextBox4)
         Controls.Add(Label5)
         Controls.Add(DateTimePicker1)
         Controls.Add(TextBox3)
         Controls.Add(TextBox2)
         Controls.Add(TextBox1)
-        Controls.Add(Button1)
         Controls.Add(Label4)
         Controls.Add(Label3)
         Controls.Add(Label2)
@@ -268,24 +302,24 @@ Partial Class ExamSchedule
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents ThoátToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents clnMMH As DataGridViewTextBoxColumn
-    Friend WithEvents clnMSV As DataGridViewTextBoxColumn
-    Friend WithEvents clnMon As DataGridViewTextBoxColumn
-    Friend WithEvents clnLop As DataGridViewTextBoxColumn
-    Friend WithEvents clnNgayThi As DataGridViewTextBoxColumn
-    Friend WithEvents clnPhong As DataGridViewTextBoxColumn
-    Friend WithEvents TạoLịchToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ThayĐổiLịchThiToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents XóaLịchToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents SắpXếpLịchThiToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents clnMMH As DataGridViewTextBoxColumn
+    Friend WithEvents clnMon As DataGridViewTextBoxColumn
+    Friend WithEvents clnLop As DataGridViewTextBoxColumn
+    Friend WithEvents clnNgayThi As DataGridViewTextBoxColumn
+    Friend WithEvents clnPhong As DataGridViewTextBoxColumn
 End Class
--- ExamAdministrator/ExamAdministrator/ExamSchedule.vb
+++ ExamAdministrator/ExamAdministrator/ExamSchedule.vb
@@ -1,5 +1,14 @@
 ﻿Public Class ExamSchedule
-    Private Sub TạoLịchToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TạoLịchToolStripMenuItem.Click
+    Private Sub TạoLịchToolStripMenuItem_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub ExamSchedule_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ThoátToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ThoátToolStripMenuItem.Click
+        Hide()
+        Dashboard.Show()
     End Sub
 End Class
