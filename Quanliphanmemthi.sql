cd043edc64d9fdce4d79490f5e4842c497ae75e2
ngotuanviet <ngotuanviet9@gmail.com>
2024-06-01 19:17:39 +0700

    Update Quanliphanmemthi.sql

--- Quanliphanmemthi.sql
+++ Quanliphanmemthi.sql
@@ -10,13 +10,11 @@ create table Sinhvien(
 	Lop nvarchar(20) not null,
 	MaKhoa nvarchar(40) not null,
 	salt nvarchar(40) not null
+	--CONSTRAINT fk_MaKhoa_Khoa_sv FOREIGN KEY (MaKhoa) REFERENCES Khoa (Makhoa)'
 )
-	use ExamDB
 insert into Sinhvien(Masv,Passsv,image,HoTen,Gioitinh,Ngaysinh,Lop,MaKhoa,salt) values 
-('2823220212','da7b628aa72d4dc9b8349bafbf9b4b7ed8df0bd4432e35392c70f27a08254d7b',(SELECT * FROM OPENROWSET(BULK 'D:\TaiLieuHocTap\TaiLieuHUBT\Modules\LTTQ\VbExam\imagesv\2823220212.jpg', SINGLE_BLOB) AS image),N'Ngô Tuấn Việt',N'Nam','2005-12-12',N'TH28.27',N'Công Nghệ Thông Tin',434282283)
+('2823220212','da7b628aa72d4dc9b8349bafbf9b4b7ed8df0bd4432e35392c70f27a08254d7b',(SELECT * FROM OPENROWSET(BULK 'C:\Users\ntv\Downloads\Compressed\VbExam\imagesv\2823220212.jpg', SINGLE_BLOB) AS image),N'Ngô Tuấn Việt',N'Nam','2005-12-12',N'TH28.27',N'CNTT',434282283)
 
-select * from Loginfo
-delete Loginfo
 create table Loginfo(
 	Tennguoidung nvarchar(60) not null,
 	Hoatdong nvarchar(40) not null,
@@ -25,7 +23,6 @@ create table Loginfo(
 	chitiet nvarchar(40)
 ) 
 insert into Loginfo(Tennguoidung,Hoatdong,Trangthai,thoigian,chitiet) values (N'Admin',N'Đăng Nhập',N'thành công',GETDATE(),null);
-use ExamDB
 create table Giangvien(
 	Magv nvarchar(60) not null primary key,
 	Passgv nvarchar(70) not null,
@@ -37,42 +34,48 @@ create table Giangvien(
 	MaKhoa nvarchar(40) not null,
 	salt nvarchar(40) not null,
 	Administrator BIT not null
+	--CONSTRAINT fk_MaKhoa_Khoa_gv FOREIGN KEY (MaKhoa) REFERENCES Khoa (Makhoa)
 )
-use ExamDB
-insert into Giangvien(Magv,Passgv,image,Hotengv,Gioitinh,Ngaysinh,Chucvu,MaKhoa,salt,Administrator) values ('GV012','da7b628aa72d4dc9b8349bafbf9b4b7ed8df0bd4432e35392c70f27a08254d7b',(SELECT * FROM OPENROWSET(BULK 'D:\TaiLieuHocTap\TaiLieuHUBT\Modules\LTTQ\VbExam\imagegv\GV012@.jpg', SINGLE_BLOB) AS image),N'Mai Quang Hải',N'Nam','07/16/2005',N'Trưởng Khoa',N'Công Nghệ Thông Tin',3456467, 1)
-select * from Loginfo
+insert into Giangvien(Magv,Passgv,image,Hotengv,Gioitinh,Ngaysinh,Chucvu,MaKhoa,salt,Administrator) values ('GV012','da7b628aa72d4dc9b8349bafbf9b4b7ed8df0bd4432e35392c70f27a08254d7b',(SELECT * FROM OPENROWSET(BULK 'C:\Users\ntv\Downloads\Compressed\VbExam\imagegv\GV012@.jpg', SINGLE_BLOB) AS image),N'Mai Quang Hải',N'Nam','07/16/2005',N'Trưởng Khoa',N'CNTT',3456467, 1)
+
+CREATE TABLE Monhoc(
+	Mamonhoc nvarchar(40) not null primary key,
+	Tenmonhoc nvarchar(60) not null,
+	MaKhoa nvarchar(40) not null,
+	SoTiet int not null
+	--CONSTRAINT fk_MaKhoa_Khoa FOREIGN KEY (MaKhoa) REFERENCES Khoa (Makhoa)
+)
+		insert into Monhoc(Mamonhoc,Tenmonhoc,MaKhoa,SoTiet) values ('TIN01','Access','CNTT',40)
+CREATE TABLE Khoa (
+	MaKhoa NVARCHAR(40) NOT NULL PRIMARY KEY,
+	TenKhoa NVARCHAR(30) NOT NULL
+)
+INSERT INTO Khoa (MaKhoa, TenKhoa) VALUES (N'CNTT', N'Công nghệ thông tin')
+
 Create table Bangdiem(
 	MaDiem nvarchar(40) not null primary key,
 	Mamonhoc nvarchar(40) not null,
 	Masv nvarchar(60) not null,
 	tenmonhoc nvarchar(60) not null,
 	Diemthi real not null,
+	--CONSTRAINT fk_Sinhvien_Bangdiem FOREIGN KEY (Masv) REFERENCES SinhVien (Masv),
+	--CONSTRAINT fk_Mamonhoc_Monhoc FOREIGN KEY (Mamonhoc) REFERENCES Monhoc (Mamonhoc)
 )
-use ExamDB
-CREATE TABLE Monhoc(
-	Mamonhoc nvarchar(40) not null primary key,
-	Tenmonhoc nvarchar(60) not null,
-	MaKhoa nvarchar(40) not null,
-	SoTiet int not null
-)
-		
-CREATE TABLE Khoa (
-	MaKhoa NVARCHAR(40) NOT NULL PRIMARY KEY,
-	TenKhoa NVARCHAR(30) NOT NULL
-)
-INSERT INTO Khoa (MaKhoa, TenKhoa) VALUES ('CNTT', N'Công nghệ thông tin')
-use ExamDB
+insert into Bangdiem(MaDiem,Mamonhoc,Masv,tenmonhoc,Diemthi) values (123,'TIN01',2823220212,N'Lý Thuyết Cơ Bản',8)
+
 CREATE TABLE DeThi (
 	MaDeThi NVARCHAR(40) NOT NULL PRIMARY KEY,
 	MaKhoa NVARCHAR(40) NOT NULL,
 	Mamonhoc NVARCHAR(40) NOT NULL,
 	SoCau int,
 	ThoiGian int,
-	HienDeThi BIT
+	HienDeThi bit
+	--CONSTRAINT fk_MamonhocDT_Monhoc FOREIGN KEY (Mamonhoc) REFERENCES MonHoc (Mamonhoc),
+	--CONSTRAINT fk_MaKhoaDT_Khoa FOREIGN KEY (MaKhoa) REFERENCES Khoa (Makhoa)
 )
 insert into DeThi(MaDeThi,MaKhoa,Mamonhoc,SoCau,ThoiGian,HienDeThi) values ('TIN01','CNTT','TIN01',0,5,1)
 
-use ExamDB
+
 CREATE TABLE CauHoi (
 	MaCauHoi NVARCHAR(40) NOT NULL PRIMARY KEY,
 	MaDeThi NVARCHAR(40) NOT NULL,
@@ -84,6 +87,8 @@ CREATE TABLE CauHoi (
 	DapAnD NVARCHAR(MAX),
 	DapAnDung int,
 	ThuTuCau int NOT NULL
+	--CONSTRAINT fk_MaDethi_Dethi FOREIGN KEY (MaDeThi) REFERENCES Dethi (MaDeThi),
+	--CONSTRAINT fk_MaAnh_ImageData FOREIGN KEY (Maanh) REFERENCES ImageData (Maanh)
 )
 
 INSERT INTO CauHoi (MaCauHoi, MaDeThi, NoiDung, DapAnA, DapAnB, DapAnC, DapAnD, DapAnDung, ThuTuCau) 
@@ -94,7 +99,7 @@ create table ImageData(
 	Maanh nvarchar(40) not null PRIMARY KEY,
 	image varbinary (max)
 )
-insert into Imagecauhoi(MaCauHoi,imagech) values ('TIN1',(SELECT * FROM OPENROWSET(BULK 'D:\TaiLieuHocTap\TaiLieuHUBT\Modules\LTTQ\VbExam\imagesv\2823220212.jpg', SINGLE_BLOB) AS image))
+insert into ImageData(Maanh,image) values ('TIN1',(SELECT * FROM OPENROWSET(BULK 'C:\Users\ntv\Downloads\Compressed\VbExam\imagegv\GV012@.jpg', SINGLE_BLOB) AS image))
 
 --CREATE TABLE DapAn (
 --	MaDapAn NVARCHAR(10) NOT NULL PRIMARY KEY,
@@ -106,6 +111,38 @@ insert into Imagecauhoi(MaCauHoi,imagech) values ('TIN1',(SELECT * FROM OPENROWS
 --		('DATIN2', 'TIN2', N'Đáp án câu 2')
 
 insert into Loginfo(Tennguoidung,Hoatdong,Trangthai,thoigian,chitiet) values (N'Admin',N'Đăng Nhập',N'thành công','2024-05-30',null);
+
+
+
+ALTER TABLE [DeThi] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
+GO
+
+ALTER TABLE [Bangdiem] ADD FOREIGN KEY ([Mamonhoc]) REFERENCES [Monhoc] ([Mamonhoc])
+GO
+
+ALTER TABLE [Bangdiem] ADD FOREIGN KEY ([Masv]) REFERENCES [Sinhvien] ([Masv])
+GO
+
+ALTER TABLE [CauHoi] ADD FOREIGN KEY ([MaDeThi]) REFERENCES [DeThi] ([MaDeThi])
+GO
+
+
+ALTER TABLE [Sinhvien] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
+GO
+
+ALTER TABLE [Giangvien] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
+GO
+
+ALTER TABLE [CauHoi] ADD FOREIGN KEY ([Maanh]) REFERENCES [ImageData] ([Maanh])
+GO
+
+ALTER TABLE [DeThi] ADD FOREIGN KEY (Mamonhoc) REFERENCES [Monhoc] (Mamonhoc)
+GO
+ALTER TABLE [Monhoc] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
+GO
+
+
+
 
 CREATE TRIGGER trg_UpdateSoCauHoiOnInsert
 ON CauHoi
@@ -124,8 +161,6 @@ BEGIN
     );
 END;
 
-select * from Loginfo
-use ExamDB
 
 CREATE TRIGGER check_account_delete
 ON Giangvien
@@ -176,120 +211,3 @@ BEGIN
     DELETE FROM Loginfo WHERE thoigian < DATEADD(WEEK, -1, GETDATE());
 END
 EXEC DeleteOldData;
-
-
-CREATE TABLE [Sinhvien] (
-  [Masv] nvarchar(60) PRIMARY KEY,
-  [Passsv] nvarchar(70),
-  [Image] varbinary(max),
-  [HoTen] Nvarchar(100),
-  [Gioitinh] nvarchar(10),
-  [Ngaysinh] date,
-  [Lop] nvarchar(20),
-  [MaKhoa] nvarchar(40),
-  [salt] nvarchar(40)
-)
-GO
-
-CREATE TABLE [Loginfo] (
-  [Tennguoidung] nvarchar(60),
-  [Hoatdong] nvarchar(40),
-  [Trangthai] nvarchar(40),
-  [thoigian] datetime,
-  [chitiet] nvarchar(40)
-)
-GO
-
-CREATE TABLE [Giangvien] (
-  [Magv] nvarchar(60) PRIMARY KEY,
-  [Passgv] nvarchar(70),
-  [Image] varbinary(max),
-  [Hotengv] nvarchar(100),
-  [Gioitinh] nvarchar(10),
-  [Ngaysinh] date,
-  [Chucvu] nvarchar(50),
-  [MaKhoa] nvarchar(40),
-  [salt] nvarchar(40),
-  [Administrator] BIT
-)
-GO
-
-CREATE TABLE [Monhoc] (
-  [Mamonhoc] nvarchar(40) PRIMARY KEY,
-  [Tenmonhoc] nvarchar(60),
-  [MaKhoa] nvarchar(40),
-  [SoTiet] int
-)
-GO
-
-CREATE TABLE [Khoa] (
-  [MaKhoa] NVARCHAR(40) PRIMARY KEY,
-  [TenKhoa] NVARCHAR(30)
-)
-GO
-
-CREATE TABLE [DeThi] (
-  [MaDeThi] NVARCHAR(40) PRIMARY KEY,
-  [MaKhoa] NVARCHAR(40),
-  [Mamonhoc] NVARCHAR(40),
-  [SoCau] int,
-  [ThoiGian] int,
-  [HienDeThi] BIT
-)
-GO
-
-CREATE TABLE [CauHoi] (
-  [MaCauHoi] NVARCHAR(40) PRIMARY KEY,
-  [MaDeThi] NVARCHAR(40),
-  [Maanh] nvarchar(40),
-  [NoiDung] NVARCHAR(MAX),
-  [DapAnA] NVARCHAR(MAX),
-  [DapAnB] NVARCHAR(MAX),
-  [DapAnC] NVARCHAR(MAX),
-  [DapAnD] NVARCHAR(MAX),
-  [DapAnDung] int,
-  [ThuTuCau] int
-)
-GO
-
-CREATE TABLE [Bangdiem] (
-  [MaDiem] nvarchar(40) PRIMARY KEY,
-  [Mamonhoc] nvarchar(40),
-  [Masv] nvarchar(60),
-  [tenmonhoc] nvarchar(60),
-  [Diemthi] real
-)
-GO
-
-CREATE TABLE [ImageData] (
-  [Maanh] nvarchar(40) PRIMARY KEY,
-  [image] varbinary(max)
-)
-GO
-
-ALTER TABLE [DeThi] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
-GO
-
-ALTER TABLE [Bangdiem] ADD FOREIGN KEY ([Mamonhoc]) REFERENCES [Monhoc] ([Mamonhoc])
-GO
-
-ALTER TABLE [Bangdiem] ADD FOREIGN KEY ([Masv]) REFERENCES [Sinhvien] ([Masv])
-GO
-
-ALTER TABLE [CauHoi] ADD FOREIGN KEY ([MaDeThi]) REFERENCES [DeThi] ([MaDeThi])
-GO
-
-ALTER TABLE [DeThi] ADD FOREIGN KEY ([Mamonhoc]) REFERENCES [Monhoc] ([Mamonhoc])
-GO
-
-ALTER TABLE [Sinhvien] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
-GO
-
-ALTER TABLE [Giangvien] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
-GO
-
-ALTER TABLE [CauHoi] ADD FOREIGN KEY ([Maanh]) REFERENCES [ImageData] ([Maanh])
-GO
-
-ALTER TABLE [Monhoc] ADD FOREIGN KEY ([MaKhoa]) REFERENCES [Khoa] ([MaKhoa])
-GO
