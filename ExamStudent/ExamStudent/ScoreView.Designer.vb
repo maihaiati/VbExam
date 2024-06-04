4a24998e6269250b77676ee8dcae3a9c7e0a3234
ngotuanviet <ngotuanviet9@gmail.com>
2024-06-04 21:13:16 +0700

    up

--- ExamStudent/ExamStudent/ScoreView.Designer.vb
+++ ExamStudent/ExamStudent/ScoreView.Designer.vb
@@ -17,82 +17,89 @@ Partial Class ScoreView
 	'Required by the Windows Form Designer
 	Private components As System.ComponentModel.IContainer
 
-	'NOTE: The following procedure is required by the Windows Form Designer
-	'It can be modified using the Windows Form Designer.  
-	'Do not modify it using the code editor.
-	<System.Diagnostics.DebuggerStepThrough()> _
-	Private Sub InitializeComponent()
-		dgScore = New DataGridView()
-		MenuStrip1 = New MenuStrip()
-		btnDashboard = New ToolStripMenuItem()
-		btnMe = New ToolStripMenuItem()
-		btnMyAccount = New ToolStripMenuItem()
-		btnLogout = New ToolStripMenuItem()
-		CType(dgScore, ComponentModel.ISupportInitialize).BeginInit()
-		MenuStrip1.SuspendLayout()
-		SuspendLayout()
-		' 
-		' dgScore
-		' 
-		dgScore.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-		dgScore.Location = New Point(12, 46)
-		dgScore.Name = "dgScore"
-		dgScore.Size = New Size(776, 392)
-		dgScore.TabIndex = 0
-		' 
-		' MenuStrip1
-		' 
-		MenuStrip1.Items.AddRange(New ToolStripItem() {btnDashboard, btnMe})
-		MenuStrip1.Location = New Point(0, 0)
-		MenuStrip1.Name = "MenuStrip1"
-		MenuStrip1.Size = New Size(800, 24)
-		MenuStrip1.TabIndex = 1
-		MenuStrip1.Text = "MenuStrip1"
-		' 
-		' btnDashboard
-		' 
-		btnDashboard.Name = "btnDashboard"
-		btnDashboard.Size = New Size(86, 20)
-		btnDashboard.Text = "Về trang chủ"
-		' 
-		' btnMe
-		' 
-		btnMe.Alignment = ToolStripItemAlignment.Right
-		btnMe.DropDownItems.AddRange(New ToolStripItem() {btnMyAccount, btnLogout})
-		btnMe.Name = "btnMe"
-		btnMe.Size = New Size(22, 20)
-		btnMe.Text = " "
-		' 
-		' btnMyAccount
-		' 
-		btnMyAccount.Name = "btnMyAccount"
-		btnMyAccount.Size = New Size(180, 22)
-		btnMyAccount.Text = "Tài khoản của tôi"
-		' 
-		' btnLogout
-		' 
-		btnLogout.Name = "btnLogout"
-		btnLogout.Size = New Size(180, 22)
-		btnLogout.Text = "Đăng xuất"
-		' 
-		' ScoreView
-		' 
-		AutoScaleDimensions = New SizeF(7F, 15F)
-		AutoScaleMode = AutoScaleMode.Font
-		ClientSize = New Size(800, 450)
-		Controls.Add(dgScore)
-		Controls.Add(MenuStrip1)
-		MainMenuStrip = MenuStrip1
-		Name = "ScoreView"
-		Text = "Exam Student - Score view"
-		CType(dgScore, ComponentModel.ISupportInitialize).EndInit()
-		MenuStrip1.ResumeLayout(False)
-		MenuStrip1.PerformLayout()
-		ResumeLayout(False)
-		PerformLayout()
-	End Sub
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ScoreView))
+        dgScore = New DataGridView()
+        MenuStrip1 = New MenuStrip()
+        btnDashboard = New ToolStripMenuItem()
+        btnMe = New ToolStripMenuItem()
+        btnMyAccount = New ToolStripMenuItem()
+        btnLogout = New ToolStripMenuItem()
+        CType(dgScore, ComponentModel.ISupportInitialize).BeginInit()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' dgScore
+        ' 
+        dgScore.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgScore.Location = New Point(14, 61)
+        dgScore.Margin = New Padding(3, 4, 3, 4)
+        dgScore.Name = "dgScore"
+        dgScore.RowHeadersWidth = 51
+        dgScore.Size = New Size(887, 523)
+        dgScore.TabIndex = 0
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(20, 20)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {btnDashboard, btnMe})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Padding = New Padding(7, 3, 0, 3)
+        MenuStrip1.Size = New Size(914, 30)
+        MenuStrip1.TabIndex = 1
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' btnDashboard
+        ' 
+        btnDashboard.Name = "btnDashboard"
+        btnDashboard.Size = New Size(106, 24)
+        btnDashboard.Text = "Về trang chủ"
+        ' 
+        ' btnMe
+        ' 
+        btnMe.Alignment = ToolStripItemAlignment.Right
+        btnMe.DropDownItems.AddRange(New ToolStripItem() {btnMyAccount, btnLogout})
+        btnMe.Name = "btnMe"
+        btnMe.Size = New Size(27, 24)
+        btnMe.Text = " "
+        ' 
+        ' btnMyAccount
+        ' 
+        btnMyAccount.Name = "btnMyAccount"
+        btnMyAccount.Size = New Size(203, 26)
+        btnMyAccount.Text = "Tài khoản của tôi"
+        ' 
+        ' btnLogout
+        ' 
+        btnLogout.Name = "btnLogout"
+        btnLogout.Size = New Size(203, 26)
+        btnLogout.Text = "Đăng xuất"
+        ' 
+        ' ScoreView
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(914, 600)
+        Controls.Add(dgScore)
+        Controls.Add(MenuStrip1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MainMenuStrip = MenuStrip1
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "ScoreView"
+        Text = "Exam Student - Score view"
+        CType(dgScore, ComponentModel.ISupportInitialize).EndInit()
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
 
-	Friend WithEvents dgScore As DataGridView
+    Friend WithEvents dgScore As DataGridView
 	Friend WithEvents MenuStrip1 As MenuStrip
 	Friend WithEvents btnDashboard As ToolStripMenuItem
 	Friend WithEvents btnMe As ToolStripMenuItem
